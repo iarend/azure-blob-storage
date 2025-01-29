668a6e475cb9b96066f402fd4811616c342da50e
Arend <i_arend@hotmail.com>
2025-01-29 19:20:05 -0300

    Endpoints get url, download e update

---
+++ AzureBlobApiTest/AzureBlobApiTest/Controller/FileController.cs
@@ -0,0 +1,30 @@
+﻿using AzureBlobApiTest.Repository.File;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureBlobApiTest.Controller;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class FileController : ControllerBase
+{
+    private readonly IFileRepository _fileRepository;
+
+    public FileController(IFileRepository fileRepository)
+    {
+        _fileRepository = fileRepository ?? throw new ArgumentNullException(nameof(fileRepository));
+    }
+
+    [HttpGet("view/{fileName}")]
+    public async Task<IActionResult> GetFileUrl(string fileName) => Ok(await _fileRepository.GetFileUrl(fileName));
+
+    [HttpGet("download/{fileName}")]
+    public async Task<IActionResult> DownloadFile(string fileName)
+    {
+        var (content, contentType) = await _fileRepository.DownloadFileAsync(fileName);
+
+        return File(content, contentType, fileName);
+    }
+
+    [HttpPost("upload")]
+    public async Task<IActionResult> UploadFile([FromBody] IFormFile file, [FromHeader] bool ignoreInapropriate) => Ok(await _fileRepository.UploadFile(file, ignoreInapropriate));
+}
